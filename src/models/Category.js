74f475a2a7bf97497205d163246a456e53b09c15
Jessica <jessica.siqueira.rj@gmail.com>
2023-12-06 21:14:09 -0300

    req 7

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+    },
+    {
+        timestamps: false,
+        underscored: true,
+        tableName: 'categories'
+      }
+    )
+    return Category;
+}
