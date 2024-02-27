5be0b5b75bef3332bdda42cff155583d2fa1d626
Jessica <jessica.siqueira.rj@gmail.com>
2024-02-27 15:02:29 -0300

    fim

--- src/migrations/20231204204217-create-user.js
+++ src/migrations/20231204204217-create-user.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('users', {
-      id: {
+      user_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         primaryKey: true,
